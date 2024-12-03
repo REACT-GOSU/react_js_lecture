bf556122fbf8448a6412af57b5283bbee680ce5b
nuyhhyun <sarahs19991115@gmail.com>
2024-12-03 14:28:56 +0900

    useEffect의 deps 사용으로 실행 기준점 정해주기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
   console.log("I run all the time.");
   useEffect(() => {
-    console.log("CALL THE API...."); // useEffect 간소화
-  }, []);
+    console.log("CALL THE API....");
+  }, []); // 변화 기준점이 없어서 한 번만 실행
+  useEffect(() => {
+    console.log("SEARCH FOR", keyword);
+  }, [keyword]); // 처음과, keyword가 변화할 때에만 해당 코드 실행, 여러 개도 가능
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
