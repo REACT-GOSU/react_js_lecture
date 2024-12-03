324f61c7235bf839f369069be012597535236f5e
nuyhhyun <sarahs19991115@gmail.com>
2024-12-03 14:04:37 +0900

    useEffect 코드 간소화

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time."); // state가 변경될 때마다 전체 코드가 실행되면서 매번 렌더
-  const iRunOnlyOnce = () => {
-    console.log("I run only once.");
-  }
-  useEffect(iRunOnlyOnce, []); // useEffect가 iRunOnlyOnce를 처음 로드 시에만 렌더
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("CALL THE API...."); // useEffect 간소화
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
