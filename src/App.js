080119e06bcb3360ef9b174743389fc5ec93d175
nuyhhyun <sarahs19991115@gmail.com>
2024-12-03 13:59:34 +0900

    useEffect로 매번 렌더되지 않도록 하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time."); // state가 변경될 때마다 전체 코드가 실행되면서 매번 렌더
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  }
+  useEffect(iRunOnlyOnce, []); // useEffect가 iRunOnlyOnce를 처음 로드 시에만 렌더
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1> {/* 같은 class명을 사용하더라도 뒤에 랜덤값을 붙여 매번 다른 클래스를 생성해냄 */}
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
