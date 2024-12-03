f95bd583589f14d68bc961a7dfd6f40c43279f79
nuyhhyun <sarahs19991115@gmail.com>
2024-12-03 13:36:35 +0900

    module을 이용한 각 컴포넌트마다의 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>Welcome back!</h1> {/* 같은 class명을 사용하더라도 뒤에 랜덤값을 붙여 매번 다른 클래스를 생성해냄 */}
       <Button text={"Continue"}/>
     </div>
   );
