568145aa98b4a13248e9bd78b31a901507541a85
nuyhhyun <sarahs19991115@gmail.com>
2024-12-07 22:40:17 +0900

    make ToDoList

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :("); // useEffect의 cleanUp: 종료될 때 설정
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); // submit으로 인한 새로고침 방지
+    if(toDo === "") { // toDo에 빈 값이 들어왔을 때
+      return; // submit 함수 killing
+    }
+    setToDos(currentArray => [toDo, ...currentArray]); // ...<array> === array의 원소들
+    setToDo(""); // input값 비우기
+  }
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1> {/* toDo 갯수 표기 */}
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => ( // text가 들어있는 array에서
+        <li key={index}>{item}</li> // list array로 각 key 설정하여 mapping
+      ))}
     </div>
-  )
+  );
 }
 
 export default App;
